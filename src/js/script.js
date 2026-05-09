fe28e9a55ddd98153d5e4b7dc909cb02e426bca1
Kevin <kevinsimoeslima@gmail.com>
2026-05-09 18:00:00 -0300

    feat: adicionando validação no formulário

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,61 @@
 const form = document.getElementById("registration-form");
 
 form.addEventListener("submit", function(event) {
+
   event.preventDefault();
+
   const nome = document.getElementById("username").value;
   const email = document.getElementById("email").value;
   const perfil = document.getElementById("profile").value;
   const descoberta = document.getElementById("discovery").value;
   const preferencia = document.getElementById("excited").value;
+
+  if (nome.trim().length < 3) {
+    alert("Digite um nome válido.");
+    return;
+  }
+
+  if (preferencia.trim().length < 5) {
+    alert("Digite no mínimo 5 letras.");
+    return;
+  }
+
+  let res = prompt(
+    "Obrigado por se registrar! Você pode compartilhar suas respostas? (Sim/Não)"
+  );
+
+  while (
+    res !== null &&
+    res.toLowerCase() !== "sim" &&
+    res.toLowerCase() !== "não"
+  ) {
+
+    alert('Digite apenas "sim" ou "não".');
+
+    res = prompt(
+      "Deseja compartilhar as respostas?"
+    );
+  }
+
+  if (res !== null && res.toLowerCase() === "sim") {
+
+    alert(
+      "Suas respostas estão compartilhadas no console.log!"
+    );
+
+    console.log("Nome:", nome);
+    console.log("Email:", email);
+    console.log("Perfil:", perfil);
+    console.log("Conheceu por:", descoberta);
+    console.log("Mais empolgado com:", preferencia);
+
+  } else {
+
+    alert(
+      "Suas respostas não serão compartilhadas no console.log."
+    );
+  }
+
   form.reset();
 
-  res = prompt("Obrigado por se registrar! Você pode compartilhar suas respostas? (Sim/Não)");
-
-  while (res.toLowerCase() !== "sim" && res.toLowerCase() !== "não") {
-    alert('Digite apenas "sim" ou "não"');
-    
-    res = prompt("Deseja compartilhar as respostas?");
-  }
-  
-  if (res.toLowerCase() === "sim") {
-    alert("Suas respostas estão compartilhadas no console.log!");
-    console.log(nome);
-    console.log(email);
-    console.log(perfil);
-    console.log(descoberta);
-    console.log(preferencia);
-  } else {
-    alert("Suas respostas não serão compartilhadas no console.log.");
-  }
 });
